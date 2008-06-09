559f171964e10910e48376546fd007c81195e931
Joshua Sierles <jsierles@joshua-sierless-macbook-air.local>
2008-06-10 00:28:55 +0200

    place actor in its own namespace rather than vertebra's

--- lib/vertebra-gem/actor.rb
+++ lib/vertebra-gem/actor.rb
@@ -1,22 +1,17 @@
-require 'vertebra/actor'
+module VertebraGem
+  class Actor  < ::Vertebra::Actor
+    
+    provides '/gem'
 
-module Vertebra
-  module Actors
-    class Gem  < ::Vertebra::Actor
-      
-      provides '/gem'
-      register_with_agent
-
-      def list(args = {})
-        filter = args['filter'] || nil
-        ::Gem.source_index.refresh!.search(filter).flatten.collect {|gemspec| "#{gemspec.name} #{gemspec.version}" }
-      end
-            
-      def install(args = {})
-        str = args['name']
-        str << "-#{args['version']}" if args['version']
-        `gem install #{str}`
-      end
+    def list(args = {})
+      filter = args['filter'] || nil
+      ::Gem.source_index.refresh!.search(filter).flatten.collect {|gemspec| "#{gemspec.name} #{gemspec.version}" }
+    end
+          
+    def install(args = {})
+      str = args['name']
+      str << "-#{args['version']}" if args['version']
+      `gem install #{str}`
     end
   end
 end
