87a665cff3aa778d364968cbb8f5bd9292a22af8
Joshua Sierles <jsierles@44.Red-88-31-135.staticIP.rima-tde.net>
2008-07-10 02:35:04 +0200

    add remove_gem operation

--- lib/vertebra-gemtool/actor.rb
+++ lib/vertebra-gemtool/actor.rb
@@ -3,6 +3,10 @@ module VertebraGemtool
     
     RESOURCES = ['/gem']
 
+    def initialize(opts, *args)
+      super
+    end
+    
     def list(args = {})
       filter = args['filter'] || nil
       ::Gem.source_index.refresh!.search(filter).flatten.collect {|gemspec| "#{gemspec.name} #{gemspec.version}"}
@@ -13,5 +17,12 @@ module VertebraGemtool
       str << "-#{args['version']}" if args['version']
       `gem install #{str}`
     end
+
+    def remove(args = {})
+      str = args['name']
+      str << "-#{args['version']}" if args['version']
+      `gem uninstall #{str}`
+    end
+
   end
 end
