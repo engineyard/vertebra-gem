13c91e65b6e2e3dcd6ee3b44064d2ed744f1d457
Joshua Sierles <jsierles@joshua-sierless-macbook-air.local>
2008-07-16 19:15:56 +0200

    add reinstall method, move Thor-related stuff into actor class

--- lib/vertebra-gemtool/actor.rb
+++ lib/vertebra-gemtool/actor.rb
@@ -9,10 +9,16 @@ require 'thor'
       super
     end
     
+    desc "list", "Get a list of gems"
+    method_options :filter => :optional
+    
     def list(args = {})
       filter = args['filter'] || nil
       ::Gem.source_index.refresh!.search(filter).flatten.collect {|gemspec| "#{gemspec.name} #{gemspec.version}"}
     end
+    
+    desc "install <gem_name>", "Install a gem"
+    method_options :name => :required
           
     def install(args = {})
       str = args['name']
@@ -20,10 +26,22 @@ require 'thor'
       `gem install #{str}`
     end
 
-    def remove(args = {})
+    desc "uninstall <gem_name>", "Install a gem"
+    method_options :name => :required
+
+    def uninstall(args = {})
       str = args['name']
       str << "-#{args['version']}" if args['version']
       `gem uninstall #{str}`
+    end
+
+    desc "reinstall <gem_name>", "Install a gem"
+    method_options :name => :required
+
+    def reinstall(args = {})
+      uninstall_output = uninstall('name' => args['name'])
+      install_output = install('name' => args['name'])
+      uninstall_output + install_output
     end
 
   end
