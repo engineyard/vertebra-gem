19a7bc3abb0872b1e60056669257096b2284055d
Joshua Sierles <jsierles@197.Red-88-31-149.staticIP.rima-tde.net>
2008-07-18 01:03:13 +0200

    add actor specs

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -1,14 +1,29 @@
 require File.dirname(__FILE__) + '/spec_helper'
+require 'vertebra-gemtool/actor'
 
 describe VertebraGemtool do
   
+  if Process.uid != 0
+    puts "Please run these tests as root."
+    exit
+  end
+     
   before(:each) do
     @options = {'name' => 'orderedhash'}
     @actor = VertebraGemtool::Actor.new(@options)
   end
-  
+
   it "should install a gem" do
-    @actor.install(@options)
-    # should we install a gem or not?
+      `gem uninstall orderedhash -I`
+      lambda { @actor.install(@options) }.should_not raise_error
+      `gem list orderedhash`.should match(/orderedhash/)        
   end
+
+  it "should uninstall a gem" do
+      `gem install orderedhash --no-rdoc --no-ri`
+      lambda { @actor.uninstall(@options) }.should_not raise_error
+      `gem list orderedhash`.should_not match(/orderedhash/)        
+      `gem install orderedhash --no-rdoc --no-ri`
+  end
+
 end
