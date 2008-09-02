562fe4c1a61b7a119de2c3a31ce313ffa5b7308d
Samuel Tesla <stesla@engineyard.com>
2008-09-02 11:26:19 -0500

    Bind ops to resources.

--- lib/vertebra-gemtool/actor.rb
+++ lib/vertebra-gemtool/actor.rb
@@ -8,7 +8,8 @@ module VertebraGemtool
 
     provides '/gem'
 
-    desc "list", "Get a list of gems"
+    bind_op "/gem/list", :list
+    desc "/gem/list", "Get a list of gems"
     method_options :filter => :optional
 
     def list(options = {})
@@ -23,7 +24,8 @@ module VertebraGemtool
       end
     end
 
-    desc "install", "Install a gem"
+    bind_op "/gem/install", :install
+    desc "/gem/install", "Install a gem"
     method_options :name => :required
 
     def install(options = {})
@@ -34,7 +36,8 @@ module VertebraGemtool
       spawn args
     end
 
-    desc "uninstall", "Install a gem"
+    bind_op "/gem/uninstall", :uninstall
+    desc "/gem/uninstall", "Install a gem"
     method_options :name => :required
 
     def uninstall(options = {})
@@ -43,21 +46,25 @@ module VertebraGemtool
       spawn "gem", "uninstall", str
     end
 
-    desc "add_source_url", "Add a rubygems source URL"
+    bind_op "/gem/source/add", :add_source_url
+    desc "/gem/source/add", "Add a rubygems source URL"
     method_options :source_url => :required
 
     def add_source_url(options = {})
       spawn "gem", "source", "-a", options['source_url']
     end
 
-    desc "remove_source_url", "Remove a rubygems source URL"
+    bind_op "/gem/source/remove", :remove_source_url
+    desc "/gem/source/remove", "Remove a rubygems source URL"
     method_options :source_url => :required
 
     def remove_source_url(options = {})
       spawn "gem", "source", "-r", options['source_url']
     end
 
-    desc "list_sources", "List rubygem sources"
+    bind_op "/gem/source/list", :list_sources
+    desc "/gem/source/list", "List rubygem sources"
+
     def list_sources(options = {})
       spawn "gem", "source", "-l" do |output|
         output.chomp.split("\n").reject { |s| s !~ /^http/ }
